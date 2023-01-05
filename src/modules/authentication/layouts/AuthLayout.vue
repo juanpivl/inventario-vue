b4e9ba33436b91e08d783ed2236f83f5e41ca4ea
unknown <juanpivl@hotmail.com>
2023-01-05 11:24:05 -0700

    layout auth terminado

--- src/modules/authentication/layouts/AuthLayout.vue
+++ src/modules/authentication/layouts/AuthLayout.vue
@@ -1,16 +1,16 @@
 <template >
   <div class="container centrado ">
-    <div class="row">
-      <div class="col-md-6 offset-md-3">
-        <h1>pagina completa del auth</h1>
+    
+      <div >
+        <h1>Bienvenido a tu inventario!</h1>
 
         <div class="d-grid gap-2 col-6 mx-auto">
-            <button @click="goToLogin"  class="btn btn-primary ">Login</button>
-            <button @click="goToRegister" class="btn btn-secondary">Registtro</button>
+            <button @click="goToLogin"  class="btn button ">Login</button>
+            <button @click="goToRegister" class="btn button">Registro</button>
         </div>
 
       </div>
-    </div>
+    
   </div>
 
   
@@ -36,9 +36,30 @@ export default {
 
 .centrado {
   position: absolute;
+  width:300px;
+  padding-right: 20px;
+  padding-left:20px;
+  padding-bottom: 15px;
+  padding-top: 10px;
+  border-radius: 20px;
+
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+  color: black;
+  background-color: white;
+  text-align: center;
+}
+
+.button{
+  color: black;
+  font-family:Arial, Helvetica, sans-serif;
+  background-color: coral;
+  
+}
+.button:hover {
+    background-color: #4CAF50; /* Green */
+    color: white;
 }
 
 
--- src/modules/authentication/views/RegisterView.vue
+++ src/modules/authentication/views/RegisterView.vue
@@ -1,24 +1,73 @@
 <template>
-  <h1>Register view</h1>
-  <input type="text" placeholder="Nombre">
-  <input type="text" placeholder="Correo">
-  <input type="password" placeholder="contraseña">
+  <div class="centrado">
+    <form class="form-horizontal">
+      <h2>Registrate</h2>
+      <div >
+        <label for="email" class="control-label col-xs-2"
+          >Correo Electronico</label
+        >
+        <div class="col-xs-10">
+          <input
+            type="text"
+            class="form-control"
+            id="email"
+            v-model="email"
+            required
+          />
+        </div>
+      </div>
+      <div >
+        <label for="password" class="control-label col-xs-2">Contraseña</label>
+        <div class="col-xs-10">
+          <input
+            type="password"
+            class="form-control"
+            id="password"
+            v-model="password"
+            required
+          />
+        </div>
+      </div>
 
-<!--Redirige al login pero luego registrara al usuario-->
-  <button @click="goToLogin">Registrar (luego se pone la funcion)</button>
+      <br />
+      <div >
+        <div class="col-xs-offset-2 col-xs-10">
+          <button type="submit" class="btn button" @click="goToLogin">
+            Registrar
+          </button>
+          |
+          <router-link to="/login" class="router-link">Ingresar</router-link>
+        </div>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    login() {
+      // Aquí iría el código para iniciar sesión
+      // utilizando el nombre de usuario y la contraseña
+      // proporcionados por el usuario
+    },
 
-    methods:{
-        goToLogin(){
-            this.$router.push({name: 'login'})
-        },
-    }
-}
+    
+      goToLogin() {
+        if (this.email && this.password) {
+          return this.$router.push({ name: "login" });
+        }
+      },
+    
+  },
+};
 </script>
 
 <style>
-
 </style>
